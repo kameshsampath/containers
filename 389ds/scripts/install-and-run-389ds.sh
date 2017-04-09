1fa1f4730cf2109107707f88522a0f4ae949bcd1
Kamesh Sampath <kamesh.sampath@hotmail.com>
2017-04-10 01:43:41 +0530

    Added kubernetes deployment

--- 389ds/scripts/install-and-run-389ds.sh
+++ 389ds/scripts/install-and-run-389ds.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -e
 
-# TODO -- Can these not be exported as DOCKER ENV variables ??
-
 export DIRSRV_HOSTNAME=${DIRSRV_HOSTNAME:-$(hostname --fqdn)}
-export DIRSRV_ADMIN_PASSWORD=${DIRSRV_ADMIN_PASSWORD:-${DIRSRV_MANAGER_PASSWORD:-"admin@123"}}
 export DIRSRV_ADMIN_USERNAME=${DIRSRV_ADMIN_USERNAME:-"admin"}
+export DIRSRV_ADMIN_PASSWORD=${DIRSRV_ADMIN_PASSWORD:-${DIRSRV_MANAGER_PASSWORD:-"admin@123"}}
 export DIRSRV_MANAGER_PASSWORD=${DIRSRV_MANAGER_PASSWORD:-${DIRSRV_ADMIN_PASSWORD:-"admin@123"}}
 export DIRSRV_SUFFIX=${DIR_SUFFIX:-"dc=example,dc=com"}
 
@@ -20,14 +18,9 @@ LOCK_DIR="/var/lock/dirsrv/slapd-dir"
 #
 setup() {
   /bin/cp -rp /etc/dirsrv-tmpl/* /etc/dirsrv
-  /sbin/setup-ds.pl -s -f /389ds-setup.inf --debug && 
-  /bin/rm -f  /389ds-setup.inf
+  /sbin/setup-ds.pl -s -f /389ds-setup.inf --debug &&
+  /bin/rm -f /389ds-setup.inf
 }
-
-if [ ! -d ${LOCK_DIR} ]; then
-   mkdir -p ${RUN_DIR} && chown -R nobody:nobody ${RUN_DIR}
-   mkdir -p ${LOCK_DIR} && chown -R nobody:nobody ${LOCK_DIR}
-fi
 
 #
 # Load the example_com domain with sample users/groups
@@ -35,16 +28,20 @@ fi
 load_example_com() {
   #start and run ns-slapd
   ns-slapd -D $BASEDIR && sleep 5
-  ldapadd -x -D"$ROOT_DN" -w${DIRSRV_MANAGER_PASSWORD} -f /tmp/users_and_groups.ldif
+  ldapadd -x -c -D"$ROOT_DN" -w${DIRSRV_MANAGER_PASSWORD} -f /tmp/users_and_groups.ldif
   pkill -f ns-slapd  && sleep 5
 }
 
-if [ ! -d ${BASEDIR} ]; then
-  /usr/local/bin/confd -onetime -backend env
-  setup
-  load_example_com
-else
-  echo "Directory server already setupm running server"
+
+if [ ! -d ${LOCK_DIR} ]; then
+   mkdir -p ${RUN_DIR} && chown -R nobody:nobody ${RUN_DIR}
+   mkdir -p ${LOCK_DIR} && chown -R nobody:nobody ${LOCK_DIR}
+fi
+
+if [ ! -d "$BASEDIR" ]; then
+   /usr/local/bin/confd -onetime -backend env
+   setup
+   load_example_com
 fi
 
 # Run the DIR Server
